1bc82890058d95d0b9022899e5ca909a22adb67b
andreaskm <andreas.krogerm@gmail.com>
2024-09-20 09:07:24 +0000

    added yiha

--- sentiment-demo/lambda/app.py
+++ sentiment-demo/lambda/app.py
@@ -3,7 +3,7 @@ import json
 import boto3
 
 def handler(event, context):
-
+    #Comment
     client = boto3.client('comprehend')
     body = event["body"]
     sentiment = client.detect_sentiment(LanguageCode = "en", Text = body)
